440e1cb1e9d98e17f0665ee294a04a60fcf7a83e
Lakshay-Saini-Git <lakshaysaini8445@gmail.com>
2024-10-08 23:33:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        myColor: '#ff6347', // Custom color
+      },
+    },
+
+  },
+  plugins: [],
+}
+
